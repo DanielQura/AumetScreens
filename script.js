1103e9d6b2c175538775c6f97fb484f8096a3f46
DanielQura <Daniel.qura@Gmail.com>
2022-06-15 23:52:11 +0300

    Add more images and Change Delay to 10 sec

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const slideshowImages = document.querySelectorAll(".slideshow img");
 
-const nextImageDelay = 8500;
+const nextImageDelay = 10000;
 let currentImageCounter = 0; 
-
 slideshowImages[currentImageCounter].style.opacity = 1;
-
 setInterval(nextImage, nextImageDelay);
 
 function nextImage() {
   slideshowImages[currentImageCounter].style.opacity = 0;
-
   currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
-
   slideshowImages[currentImageCounter].style.opacity = 1;
-  
 }
