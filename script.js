615f13a0cb215b0635995b0badaf206de029727e
DanielQura <Daniel.qura@Gmail.com>
2022-06-15 10:14:10 +0300

    Remove Refresh

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 const slideshowImages = document.querySelectorAll(".slideshow img");
 
 const nextImageDelay = 8500;
-let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
+let currentImageCounter = 0; 
 
-// slideshowImages[currentImageCounter].style.display = "block";
 slideshowImages[currentImageCounter].style.opacity = 1;
 
 setInterval(nextImage, nextImageDelay);
 
 function nextImage() {
-  // slideshowImages[currentImageCounter].style.display = "none";
   slideshowImages[currentImageCounter].style.opacity = 0;
 
   currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
 
-  // slideshowImages[currentImageCounter].style.display = "block";
   slideshowImages[currentImageCounter].style.opacity = 1;
-
-
-
   
-
 }
