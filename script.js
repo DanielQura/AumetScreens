8ed25a1f50c053f54bfb2682215f9ab3692b6812
DanielQura <Daniel.qura@Gmail.com>
2022-06-16 11:37:04 +0300

    ds

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// const slideshowImages = document.querySelectorAll(".slideshow img");
+const slideshowImages = document.querySelectorAll(".slideshow img");
 
-// const nextImageDelay = 5000;
-// let currentImageCounter = 0; 
-// slideshowImages[currentImageCounter].style.opacity = 1;
-// setInterval(nextImage, nextImageDelay);
+const nextImageDelay = 85000;
+let currentImageCounter = 0; 
+slideshowImages[currentImageCounter].style.opacity = 1;
+setInterval(nextImage, nextImageDelay);
 
-// function nextImage() {
-//   slideshowImages[currentImageCounter].style.opacity = 0;
-//   currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
-//   slideshowImages[currentImageCounter].style.opacity = 1;
-// }
+function nextImage() {
+  slideshowImages[currentImageCounter].style.opacity = 0;
+  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
+  slideshowImages[currentImageCounter].style.opacity = 1;
+}
