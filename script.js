2ae29896d8037876f3459beea69db5b2c551e671
DanielQura <Daniel.qura@Gmail.com>
2022-06-16 08:42:42 +0300

    edit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const slideshowImages = document.querySelectorAll(".slideshow img");
 
-const nextImageDelay = 5000;
+const nextImageDelay = 8500;
 let currentImageCounter = 0; 
 slideshowImages[currentImageCounter].style.opacity = 1;
 setInterval(nextImage, nextImageDelay);
